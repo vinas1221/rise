42f05d7d730125d5fc5e88a4898811d99a802863
vinas1221 <benjo_vinas@yahoo.com>
2025-01-28 07:41:21 +0000

    Refactor configuration - fixing typo

--- src/react/useBlobity.ts
+++ src/react/useBlobity.ts
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react';
 import Blobity, { Options } from '../Blobity';
 
 export default (options: Partial<Options>) => {
-    let instance = useRef<Blobity | null>(null);
+    const instance = useRef<Blobity | null>(null);
 
     useEffect(() => {
         if (!instance.current) {
