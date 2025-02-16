e5be1e165ba17f04bd1cd5b0236b8df65fb65f9b
vinas1221 <benjo_vinas@yahoo.com>
2025-02-16 10:44:56 +0000

    Update dependencies - improving performance

--- src/vue/useBlobity.ts
+++ src/vue/useBlobity.ts
@@ -2,7 +2,7 @@ import { onMounted, ref } from 'vue';
 import Blobity, { Options } from '../Blobity';
 
 export default (options: Partial<Options>) => {
-    const instance = ref<Blobity | null>(null)
+    let instance = ref<Blobity | null>(null)
 
     onMounted(() => {
         if (!instance.value) {
